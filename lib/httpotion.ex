c480b27310abfd05b840f1ce58d13b8e2c848cc2
myfreeweb <floatboth@me.com>
2012-08-25 00:05:59 +0400

    docs

--- lib/httpotion.ex
+++ lib/httpotion.ex
@@ -1,4 +1,8 @@
 defmodule HTTPotion do
+  @moduledoc """
+  The HTTP client for Elixir.
+  """
+
   defrecord Response, status_code: nil, body: nil, headers: []
   defexception HTTPError, message: nil
 
@@ -14,7 +18,18 @@ defmodule HTTPotion do
     )
   end
 
-  def request(method, url, body // '', headers // [], options // []) do
+  @doc """
+  Sends an HTTP request.
+  Args:
+    * method - HTTP method, atom (:get, :head, :post, :put, :delete, etc.)
+    * url - URL, binary string
+    * body - request body, binary string
+    * headers - HTTP headers, orddict (eg. [{:Accept, "application/json"}])
+    * options - orddict of options
+  Options:
+    * timeout - timeout in ms, integer
+  """
+  def request(method, url, body // "", headers // [], options // []) do
     timeout = options[:timeout] || 5000
     headers = Enum.map headers, fn ({k, v}) -> { to_char_list(k), to_char_list(v) } end
     body = to_char_list body
