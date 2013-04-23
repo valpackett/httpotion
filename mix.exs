717e7842facaec5ce4d79a4d6c234a9eda9333ea
myfreeweb <floatboth@me.com>
2013-04-23 21:46:39 +0400

    add package, fix tests

--- mix.exs
+++ mix.exs
@@ -7,16 +7,13 @@ defmodule HTTPotion.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
   def application do
     [applications: [:ibrowse]]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [
-      {:ibrowse, "4.0.1", git: "https://github.com/cmullaparthi/ibrowse.git"}
+      {:ibrowse, "4.0.1", github: "cmullaparthi/ibrowse"}
     ]
   end
 end
---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "httpotion", description: "Fancy HTTP client for Elixir",
+                 version: "0.1.0", keywords: ["http", "web", "client", "network"],
+                 maintainers: [[name: "Greg V",
+                                email: "floatboth@me.com"]],
+                 repositories: [[github: "myfreeweb/httpotion"]])
