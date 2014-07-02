a6bcbbac7b6f15ee93c5c941de36d59ef8c85743
myfreeweb <floatboth@me.com>
2014-07-02 21:07:31 +0400

    release 0.2.4

--- mix.exs
+++ mix.exs
@@ -3,16 +3,31 @@ defmodule HTTPotion.Mixfile do
 
   def project do
     [app: :httpotion,
-     version: "0.2.3",
+     version: "0.2.4",
      elixir:  "~> 0.13.3 or ~> 0.14.0",
-     deps: deps]
+     description: description,
+     deps: deps,
+     package: package]
   end
 
   def application do
     [applications: [:ssl, :ibrowse]]
   end
 
+  defp description do
+    """
+    Fancy HTTP client for Elixir, based on ibrowse.
+    """
+  end
+
   defp deps do
     [{ :ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.0" }]
+  end
+
+  defp package do
+    [files: ["lib", "priv", "mix.exs", "README.md", "COPYING"],
+     contributors: ["Greg V", "Aleksey Magusev"],
+     licenses: ["Do What the Fuck You Want to Public License, Version 2"],
+     links: %{ "GitHub" => "https://github.com/myfreeweb/httpotion" }]
   end
 end
