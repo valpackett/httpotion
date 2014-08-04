13e0490030a9ecafbba39f957204ca8fe1180ff9
Aleksei Magusev <lexmag@me.com>
2014-08-05 01:05:51 +0400

    Remove `package.exs` (Expm is no longer maintained)

--- package.exs
+++
@@ -1,9 +0,0 @@
-Expm.Package.new(name: "httpotion", description: "Fancy HTTP client for Elixir",
-                 version: "0.2.4", keywords: ["http", "web", "client", "network"],
-                 dependencies: ["ssl", "ibrowse"],
-                 licenses: [[name: "Do What the Fuck You Want to Public License, Version 2", file: "COPYING"]],
-                 maintainers: [[name: "Greg V",
-                                email: "floatboth@me.com"],
-                               [name: "Aleksey Magusev",
-                                email: "lexmag@me.com"]],
-                 repositories: [[github: "myfreeweb/httpotion"]])
