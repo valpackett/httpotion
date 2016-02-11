3e477801650ab30fb95808fb54e7be36581d9d6d
Greg V <greg@unrelenting.technology>
2016-02-12 01:14:35 +0300

    mix stuff

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule HTTPotion.Mixfile do
       source_url: "https://github.com/myfreeweb/httpotion",
       version: "2.1.0",
       elixir:  "~> 1.0",
-      docs: [extras: ["README.md"]],
+      docs: [ extras: ["README.md"] ],
       description: description,
       deps: deps,
       package: package ]
@@ -25,13 +25,13 @@ defmodule HTTPotion.Mixfile do
 
   defp deps do
     [ {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2"},
-      {:ex_doc, "~> 0.8", only: :dev} ]
+      {:ex_doc, "~> 0.11", only: [:dev, :test]} ]
   end
 
   defp package do
-    [ files: ["lib", "mix.exs", "README.md", "UNLICENSE"],
-      contributors: ["Greg V", "Aleksei Magusev", "pragdave", "Adam Kittelson", "Ookami Kenrou", "Sumeet Singh", "Guillermo Iguaran", "jadlr", "parroty", "Daniel Berkompas", "Florian J. Breunig", "Gabe Kopley", "Arjan van der Gaag", "Joseph Wilk", "Aidan Steele", "Paulo Almeida", "Peter Hamilton", "Rachel Bowyer", "Steve", "Wojciech Kaczmarek", "d0rc", "falood", "Arkar Aung", "Dave Thomas", "Eduardo Gurgel", "Eito Katagiri", "Everton Ribeiro"],
-      licenses: ["Unlicense", "Do What the Fuck You Want to Public License, Version 2"],
+    [ files: [ "lib", "mix.exs", "README.md", "UNLICENSE" ],
+      contributors: [ "Greg V", "Aleksei Magusev", "thilko", "pragdave", "Adam Kittelson", "Greg", "Ookami Kenrou", "Guillermo Iguaran", "jadlr", "Sumeet Singh", "Hugo Ribeira", "parroty", "Daniel Berkompas", "Arkar Aung", "Henrik Nyh", "Joseph Wilk", "Low Kian Seong", "Nick", "Aidan Steele", "Paulo Almeida", "Peter Hamilton", "Rachel Bowyer", "Steve", "Strand McCutchen", "Syohei YOSHIDA", "Tomos John Rees", "Wojciech Kaczmarek", "d0rc", "falood", "Eduardo Gurgel", "Dave Thomas", "Eito Katagiri", "Everton Ribeiro", "Florian J. Breunig", "Gabe Kopley", "Arjan van der Gaag" ],
+      licenses: [ "Unlicense", "Do What the Fuck You Want to Public License, Version 2" ],
       links: %{ "GitHub" => "https://github.com/myfreeweb/httpotion" } ]
   end
 end
