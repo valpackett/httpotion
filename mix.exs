2900bc97805b4dd73562cf194ccee2f572da488a
Greg V <greg@unrelenting.technology>
2016-04-15 11:33:04 +0300

    lower elixir version requirement to 1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule HTTPotion.Mixfile do
       name: "httpotion",
       source_url: "https://github.com/myfreeweb/httpotion",
       version: "2.2.2",
-      elixir:  "~> 1.2",
+      elixir:  "~> 1.1",
       docs: [ extras: ["README.md", "CODE_OF_CONDUCT.md"] ],
       description: description,
       deps: deps,
