e4230cc56a005903d85e50f804d065f24e21de7c
Greg V <greg@unrelenting.technology>
2018-02-10 18:43:12 +0300

    release 3.1.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule HTTPotion.Mixfile do
     [ app: :httpotion,
       name: "httpotion",
       source_url: "https://github.com/myfreeweb/httpotion",
-      version: "3.0.3",
+      version: "3.1.0",
       elixir:  "~> 1.3",
       docs: [ extras: ["README.md", "CODE_OF_CONDUCT.md"] ],
       description: description(),
