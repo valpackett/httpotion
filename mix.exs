bff97a73bd9c1e67e18f21ce12ed425f383c6af1
unrelentingtech <hello@unrelenting.technology>
2022-08-02 15:50:32 +0300

    release 3.2.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule HTTPotion.Mixfile do
     [ app: :httpotion,
       name: "httpotion",
       source_url: "https://github.com/unrelentingtech/httpotion",
-      version: "3.1.3",
+      version: "3.2.0",
       elixir:  "~> 1.3",
       docs: [ extras: ["README.md", "CODE_OF_CONDUCT.md"] ],
       description: description(),
